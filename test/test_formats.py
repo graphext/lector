7c74ed129563d8d99a5831862f91e75eae45917f
Thomas Buhrmann <thomas@graphext.com>
2022-06-15 19:37:33 +0200

    Test complete format detection.

--- test/test_formats.py
+++ test/test_formats.py
@@ -1,34 +1,110 @@
 """Test integration/interactions between encoding, preambles, dialect."""
+from __future__ import annotations
+
 import io
+from csv import get_dialect
 
 import pytest
 from hypothesis import given
-from hypothesis_csv.strategies import csv as make_csv
+from hypothesis.strategies import data
+from hypothesis_csv.strategies import csv as csv_strat
 
-from lector.csv.abc import EmptyFileError, Reader
+from lector.csv import Dialect, EmptyFileError, Reader
 
-# from lector.log import LOG
+from .test_dialects import fix_expected_dialect
+from .test_encodings import CODECS, codecs_compatible
+from .utils import equal
+
+PREAMBLES = [
+    # Brandwatch, the delimiter should be ignored in preamble, as long as a row of commas
+    # separates the preamble from CSV table
+    ("a{d}b{d}c\n,,", 2),
+    ("some text\n,,", 2),
+    ("a{d}b{d}c\nsome text\n,,", 3),
+    # Fieldless
+    # Start with header line having multiple (two) fields (delimited)
+    ("abc{d}def", 0),
+    ('"abc"{d}"def"', 0),
+    ('"abc"{d}def', 0),
+    ('abc{d} "123,456"', 0),
+    ('"[cat1, cat2]"{d} "123,67"', 0),
+    # Since the line isn't quoted as a whole, the delimiter creates two fields
+    ('A line that has "something{d}in quotes" and then some', 0),
+    ('A line that has ""something{d} in quotes"" and then some', 0),
+    # Start with line having single field
+    ("abc", 1),
+    ('"abc, def"', 1),
+    ('"The ""text,"" is double-quoted, and contains a comma"', 1),
+    ('"A line that has ""something, in quotes"" and then some"', 1),
+    ('"A line that has something, and then some more"', 1),
+    ('"One line, spreading over\n multiple, lines"', 2),
+    ("One line\nAnother line", 2),
+    ('One line\nAnother line\n"And, a\nthird"', 4),
+]
+
+
+def with_delimiter(s, delim):
+    if "{d}" in s:
+        return s.format(d=delim)
+    return s
 
 
 class NonParser(Reader):
+    """Simple implementation of abstract class that only infers format."""
+
     def parse(self, *args, **kwds) -> None:
         return None
 
 
-def analyze_string(csv: str):
-    csv = io.StringIO(csv)
-    reader = NonParser(csv)
+def detect_format(csv: str | bytes):
+    if isinstance(csv, str):
+        buffer = io.StringIO(csv)
+    else:
+        buffer = io.BytesIO(csv)
+
+    reader = NonParser(buffer, log=False)
     reader.read()
     return reader.format
 
 
 def test_empty_file():
     with pytest.raises(EmptyFileError):
-        analyze_string("")
+        detect_format("")
 
 
-@given(csv=make_csv(lines=10, header=5))
-def test_dialect(csv: str):
-    reader = analyze_string(csv)
-    # dialect = reader.dialect
-    assert len(reader.columns) == 5
+@given(data=data())
+@pytest.mark.parametrize("codec", CODECS)
+@pytest.mark.parametrize("preamble", PREAMBLES)
+@pytest.mark.parametrize("dialect", ["excel", "excel-tab", "unix"])
+def test_formats(codec, preamble, dialect, data):
+    """Test parsing with different encoding, dialect and preamble."""
+
+    pydialect = get_dialect(dialect)
+
+    # Make premable compatible with dialect & generated csv
+    preamble, skiprows = preamble
+    preamble = with_delimiter(preamble, pydialect.delimiter)
+    preamble = preamble.replace("\n", pydialect.lineterminator)
+    if not pydialect.skipinitialspace:
+        preamble = preamble.replace(f"{pydialect.delimiter} ", f"{pydialect.delimiter}")
+
+    # Create the full, encoded CSV "file"
+    strategy = csv_strat(dialect=pydialect, lines=3, header=2)
+    csv = data.draw(strategy)
+    csv = preamble + pydialect.lineterminator + csv
+    # LOG.print(csv)
+
+    try:
+        encoded = csv.encode(codec)
+        format = detect_format(encoded)
+
+        # We have very short CSVs with preambles including the ñ, whose encoding
+        # cannot always be detected correctly (would be easier with larger examples)
+        assert codecs_compatible(codec, format.encoding, encoded, n_err_max=1)
+        assert equal(skiprows, format.preamble, extra=csv)
+
+        exp_dialect = fix_expected_dialect(Dialect.from_builtin(pydialect))
+        assert equal(exp_dialect, format.dialect, extra=csv)
+
+    except UnicodeEncodeError:
+        pass
